c19d892419dc914ff4000f8fd915c2f81a7354b6
Serge Broslavsky <serge.broslavsky@gmail.com>
2012-08-15 21:04:53 +0300

    Adding configuration file processing - WIP.

---
+++ src/database.c
@@ -0,0 +1,32 @@
+/* yourTime
+ * 
+ * Database implementation (SQLite based).
+ *
+ * TODO: complete heared (license, author, etc)
+ */
+
+#include <sqlite.h>
+#include <error.h>
+
+#include "database.h"
+
+int db_init(Database *db)
+{
+    return -1;
+}
+
+int db_open(Database *db, char *file)
+{
+    return -1;
+}
+
+int db_close(Database *db)
+{
+    return -1;
+}
+
+int db_add_event(Database *db, Event *event)
+{
+    return -1;
+}
+
---
+++ src/database.h
@@ -0,0 +1,48 @@
+/* yourTime
+ * 
+ * Database implementation (SQLite based).
+ *
+ * TODO: complete header (license, author, etc)
+ */
+
+#pragma once
+
+#include <stdlib.h>
+#include <sqlite.h>
+#include <time.h>
+
+#include "config.h"
+
+/* Event
+ *
+ * Represents a change in activities.
+ */
+
+struct Event
+{
+    int             id;
+    time_t          timestamp;
+    char            *summary;
+};
+
+typedef struct _Event Event;
+
+
+
+/* Database
+ *
+ * Database abstraction.
+ */
+
+struct _Database {
+    char            *file_name;
+    sqlite*         db;
+};
+
+typedef struct _Database Database;
+
+int db_init(Database *db);
+int db_open(Database *db, char *file);
+int db_close(Database *db);
+int db_add_event(Database *db, Event *event);
+
