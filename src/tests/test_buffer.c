97bbbc320a5ba40cce546ca8affb4e9c2738ba46
Serge Broslavsky <serge.broslavsky@gmail.com>
2012-09-13 09:13:28 +0300

    Added more test placeholders (to be filled with real tests).

---
+++ src/tests/test_buffer.c
@@ -0,0 +1,100 @@
+/* yourTime
+ *
+ * Buffer test set file.
+ *
+ * TODO: complete header (license, author, etc)
+ */
+
+#define _XOPEN_SOURCE 600
+
+#include "test.h"
+#include "../buffer.h"
+
+/* Test for allocation routine.
+ *
+ */
+
+START_TEST(buffer, buffer_alloc)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for data appending routine.
+ *
+ */
+
+START_TEST(buffer, buffer_append)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for used space aize query routine.
+ *
+ */
+
+START_TEST(buffer, buffer_used)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for getting buffer contents a C string routine.
+ *
+ */
+
+START_TEST(buffer, buffer_get_as_string)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for string deallocation routine.
+ *
+ */
+
+START_TEST(buffer, buffer_free_string)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for buffer tip moving routine.
+ *
+ */
+
+START_TEST(buffer, buffer_seek)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for buffer trimming routine.
+ *
+ */
+
+START_TEST(buffer, buffer_trim)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for deallocation routine.
+ *
+ */
+
+START_TEST(buffer, buffer_free)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
---
+++ src/tests/test_string.c
@@ -0,0 +1,68 @@
+/* yourTime
+ *
+ * String test set file.
+ *
+ * TODO: complete header (license, author, etc)
+ */
+
+#define _XOPEN_SOURCE 600
+
+#include "test.h"
+#include "../string.h"
+
+
+/* Test for allocation routine.
+ *
+ */
+
+START_TEST(string, string_allocate)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for static allocation routine.
+ *
+ */
+
+START_TEST(string, string_allocate_static)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for deallocation routines.
+ *
+ */
+
+START_TEST(string, string_release)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for copy routine.
+ *
+ */
+
+START_TEST(string, string_copy)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for hold routine.
+ *
+ */
+
+START_TEST(string, string_hold)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
