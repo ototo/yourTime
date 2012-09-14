a299d426f27cd813986ba2b17d3b96d114530622
Serge Broslavsky <serge.broslavsky@gmail.com>
2012-09-14 15:00:33 +0300

    Test placeholders for database.

---
+++ src/tests/test_database.c
@@ -0,0 +1,176 @@
+/* yourTime
+ *
+ * Database test set file.
+ *
+ * TODO: complete header (license, author, etc)
+ */
+
+#define _XOPEN_SOURCE 600
+
+#include "test.h"
+#include "../database.h"
+
+/* Test for initialization routine.
+ *
+ */
+
+START_TEST(database, db_init)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for uninitialization routine.
+ *
+ */
+
+START_TEST(database, db_free)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for opening routine.
+ *
+ */
+
+START_TEST(database, db_open)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for status check routine.
+ *
+ */
+
+START_TEST(database, db_is_open)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for closing routine.
+ *
+ */
+
+START_TEST(database, db_close)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for schema creating routine.
+ *
+ */
+
+START_TEST(database, db_create_schema)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for schema updating routine.
+ *
+ */
+
+START_TEST(database, db_update_schema)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for schema check routine.
+ *
+ */
+
+START_TEST(database, db_check_schema)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for activity starting routine.
+ *
+ */
+
+START_TEST(database, db_start_activity)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for activity switching routine.
+ *
+ */
+
+START_TEST(database, db_switch_activity)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for activity stopping routine.
+ *
+ */
+
+START_TEST(database, db_stop_activity)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for string allocation routine.
+ *
+ */
+
+START_TEST(database, db_alloc_string)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for string unallocation routine.
+ *
+ */
+
+START_TEST(database, db_free_string)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for activity allocation routine.
+ *
+ */
+
+START_TEST(database, db_alloc_activities)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
+
+/* Test for activity unallocation routine.
+ *
+ */
+
+START_TEST(database, db_free_activities)
+
+    TEST_NOT_IMPLEMENTED;
+
+END_TEST
+
